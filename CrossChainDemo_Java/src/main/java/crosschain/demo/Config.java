362a9eb8fe74283cdd7f2597a91de3c2c53aef84
bettybao1209 <baoting@onchain.com>
2021-06-01 18:13:30 +0800

    add demo in java

---
+++ CrossChainDemo_Java/src/main/java/crosschain/demo/Config.java
@@ -0,0 +1,50 @@
+package crosschain.demo;
+
+import io.neow3j.contract.ScriptHash;
+import io.neow3j.protocol.Neow3j;
+import io.neow3j.protocol.http.HttpService;
+import io.neow3j.wallet.Account;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationProperties
+@Configuration
+@Data
+public class Config {
+
+    private String rpcUrl;
+    private String wif;
+    private String proxyHashStr;
+    private String nNeoHashStr;
+    private String N3ReceiveAddressStr;
+    private String N3Id;
+
+    @Bean
+    public Neow3j neow3j(){
+        return Neow3j.build(new HttpService(rpcUrl));
+    }
+
+    // Replace with your wif string
+    @Bean
+    public Account account(){
+        return Account.fromWIF(wif).build();
+    }
+
+    @Bean
+    public ScriptHash proxyHash() {
+        return new ScriptHash(proxyHashStr);
+    }
+
+    @Bean
+    public ScriptHash nNeoHash(){
+        return new ScriptHash(nNeoHashStr);
+    }
+
+    // Replace with your address
+    @Bean
+    public ScriptHash N3ReceiveAddress(){
+        return new ScriptHash(N3ReceiveAddressStr);
+    }
+}
---
+++ CrossChainDemo_Java/src/main/java/crosschain/demo/Demo.java
@@ -0,0 +1,69 @@
+package crosschain.demo;
+
+import io.neow3j.contract.ContractInvocation;
+import io.neow3j.contract.ContractParameter;
+import io.neow3j.crypto.transaction.RawTransactionOutput;
+import io.neow3j.model.types.NEOAsset;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+@RestController
+public class Demo {
+
+	@Autowired
+	Config config;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo.class, args);
+	}
+
+	// amount should carry the decimal of 8, that is, input 10_00000000 for 10 nneo
+	// You should call the mint function first to convert utxo-neo to nneo
+	@PostMapping("/migrate")
+	public void migrateToken(@RequestParam int amount){
+		try {
+			ContractInvocation invoc = new ContractInvocation.Builder(config.neow3j())
+					.contractScriptHash(config.proxyHash())
+					.function("lock")
+					.parameters(Arrays.asList(
+							ContractParameter.hash160(config.nNeoHash()),// asset to be transferred
+							ContractParameter.hash160(config.account().getScriptHash()), //sender's address in little-endian
+							ContractParameter.integer(Integer.valueOf(config.getN3Id())), // N3 chainId
+							ContractParameter.hash160(config.N3ReceiveAddress()), // recipient's address in little-endian
+							ContractParameter.integer(amount) //asset amount to be locked
+					))
+					.account(config.account())
+					.build()
+					.sign()
+					.invoke();
+			System.out.println(invoc.getTransaction().getTxId());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	@PostMapping("/mint")
+	public void mintToken(@RequestParam double lockValue){
+		try {
+			config.account().updateAssetBalances(config.neow3j());
+			ContractInvocation invoc = new ContractInvocation.Builder(config.neow3j())
+					.contractScriptHash(config.nNeoHash())
+					.function("mintTokens")
+					.account(config.account())
+					.output(new RawTransactionOutput(NEOAsset.HASH_ID, lockValue, config.nNeoHash().toAddress()))
+					.build()
+					.sign()
+					.invoke();
+			System.out.println(invoc.getTransaction().getTxId());
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
